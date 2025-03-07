124f3373429b15248b239087e2b43e7321c150ec
Rurizora <bottoramku@gmail.com>
2025-03-08 04:19:09 +0700

    Update ig.js

--- api/ig.js
+++ api/ig.js
@@ -1,25 +1,23 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
-
-
 const ig = async (url,baseUrl) =>{
 let time = new Date()
 // Menyiapkan data form
 const formData = new FormData();
-formData.append('link', url);
-formData.append('downloader', 'video');
-formData.append('pageLoad', time);
+formData.append('url', url);
+formData.append('new', '2');
+formData.append('lang', 'en');
+formData.append('app', '');
 
 const config = {
   headers: {
-    'Accept': 'application/json, text/javascript, */*; q=0.01',
-    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
     'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Cookie': 'PHPSESSID=5rl20jre4776743m6np8lo34m0',
-    'Host': 'indownloader.app',
-    'Origin': 'https://indownloader.app',
-    'Referer': 'https://indownloader.app/video-downloader',
+    'Content-Length': '477',
+    'Content-Type': 'multipart/form-data',
+    'Origin': 'https://snapinsta.app',
+    'Priority': 'u=1, i',
+    'Referer': 'https://snapinsta.app/',
     'Sec-CH-UA': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
     'Sec-CH-UA-Mobile': '?0',
     'Sec-CH-UA-Platform': '"Windows"',
@@ -27,18 +25,16 @@ const config = {
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest'
   }
 };
 
 try{
 
-let respon = await axios.post('https://indownloader.app/request', formData, config)
+let respon = await axios.post('https://snapinsta.app/get-data.php', formData, config)
 if(respon.status == 200){
-if(respon.data.error)return {status: false, author: "iwan", message: "Gagal mengunduh"}
-let $ = cheerio.load(respon.data.html)
-let thumb = $('.post-thumb > img').attr('src')
-let link = $('.download-options > a').attr('href')
+
+let thumb = respon.data.files[0].thumbnail_url
+let link = respon.data.files[0].video_url
 return {status: true, author: "iwan", result:{thumb:thumb, link:link,linkUnblock:`${baseUrl}/download/id?data=${encodeURIComponent(link)}`}}
 }else{
   return {status: false, author: "iwan", message: "Gagal mengunduh"}
