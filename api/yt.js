338c06dbdaa1dca8678a30c9bf67cd0ccaa29a22
wansky05 <115464683+wansky05@users.noreply.github.com>
2025-03-08 05:10:10 +0700

    Create yt.js

---
+++ api/yt.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const ig = async (url,baseUrl) =>{
+let time = new Date()
+function getYouTubeVideoId(url) {
+	const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|v\/|embed\/|user\/[^\/\n\s]+\/)?(?:watch\?v=|v%3D|embed%2F|video%2F)?|youtu\.be\/|youtube\.com\/watch\?v=|youtube\.com\/embed\/|youtube\.com\/v\/|youtube\.com\/shorts\/|youtube\.com\/playlist\?list=)([a-zA-Z0-9_-]{11})/;
+	const match = url.match(regex);
+	return match ? match[1] : null;
+}
+let videoID = await getYouTubeVideoId(url)
+// Menyiapkan data json
+const jsondata ={"youtube_id":videoID,"quality":4,"formatValue":1}
+
+const config = {
+  headers: {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type": "application/json",
+    "Origin": "https://cnvmp3.com",
+    "Priority": "u=1, i",
+    "Referer": "https://cnvmp3.com/v23",
+    "Sec-CH-UA": '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+    "Sec-CH-UA-Mobile": "?0",
+    "Sec-CH-UA-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+  }
+};
+
+try{
+
+let respon = await axios.post('https://snapinsta.app/get-data.php', formData, config)
+if(respon.status == 200){
+if(respon.data == '')return {status: false, author: "iwan", message: "Gagal mengunduh"}
+return {status: true, author: "iwan", result:{title:respon.title, link:respon.data.server_path,linkUnblock:`${baseUrl}/download/id?data=${encodeURIComponent(respon.data.server_path)}`}}
+}else{
+  return {status: false, author: "iwan", message: "Gagal mengunduh"}
+}
+}catch(e){
+  return {status: false, author: "iwan", message: e.message}
+}
+}
+
+module.exports = ig;
