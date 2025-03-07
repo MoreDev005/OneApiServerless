6a71b74a2d2a050877c826db368d5b0d79d1f692
wansky05 <115464683+wansky05@users.noreply.github.com>
2025-03-08 05:47:21 +0700

    Update yt.js

--- api/yt.js
+++ api/yt.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const ig = async (url,baseUrl) =>{
+const yt = async (url,baseUrl) =>{
 let time = new Date()
 function getYouTubeVideoId(url) {
 	const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|v\/|embed\/|user\/[^\/\n\s]+\/)?(?:watch\?v=|v%3D|embed%2F|video%2F)?|youtu\.be\/|youtube\.com\/watch\?v=|youtube\.com\/embed\/|youtube\.com\/v\/|youtube\.com\/shorts\/|youtube\.com\/playlist\?list=)([a-zA-Z0-9_-]{11})/;
@@ -30,9 +30,21 @@ const config = {
 };
 
 try{
-
-let respon = await axios.post('https://snapinsta.app/get-data.php', formData, config)
+let respon = await axios.post('https://cnvmp3.com/check_database.php', jsondata, config)
 if(respon.status == 200){
+if(respon.data.success == false){
+let profildata = {"url":`https://www.youtube.com/watch?v=${videoID}`,"token":"1234"}
+let get_video_data = await axios.post('https://cnvmp3.com/get_video_data.php', profildata, config)
+if(get_video_data.data.succes){
+let ucep = {"url":`https://www.youtube.com/watch?v=${videoID}`,"quality":4,"title":get_video_data.data.title,"formatValue":1}
+let download_video_ucep = await axios.post('https://cnvmp3.com/download_video_ucep.php', ucep, config)
+if(download_video_ucep.data.succes){
+let insertdb = {"youtube_id":videoID,"server_path":download_video_ucep.data.download_link,"quality":4,"title":get_video_data.data.title,"formatValue":1}
+await axios.post('https://cnvmp3.com/download_video_ucep.php', inserdb, config)
+return {status: true, author: "iwan", result:{title:get_video_data.data.title, link:download_video_ucep.data.download_link,linkUnblock:`${baseUrl}/download/id?data=${encodeURIComponent(download_video_ucep.data.download_link)}`}}
+}
+}
+}	
 if(respon.data == '')return {status: false, author: "iwan", message: "Gagal mengunduh"}
 return {status: true, author: "iwan", result:{title:respon.title, link:respon.data.server_path,linkUnblock:`${baseUrl}/download/id?data=${encodeURIComponent(respon.data.server_path)}`}}
 }else{
@@ -43,4 +55,4 @@ return {status: true, author: "iwan", result:{title:respon.title, link:respon.da
 }
 }
 
-module.exports = ig;
+module.exports = yt;
