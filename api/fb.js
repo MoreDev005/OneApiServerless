9ec3288ea7d60f3032133f21782e79ec5f8237bd
Rurizora <bottoramku@gmail.com>
2025-03-07 17:50:16 +0700

    Update fb.js

--- api/fb.js
+++ api/fb.js
@@ -27,7 +27,7 @@ let $ = cheerio.load(respon.data.template)
  return {
  status:true,
  author:"iwan",
- result:{thumb:thumb,videohd:hd,videosd:sd}
+ result:{thumb:thumb,link:{hd:arrlink[0],sd:arrlink[1]},unblock:{hd:hd,sd:sd}}
  }
 }catch(e){
   return {status:false ,author:"iwan",message:e.message}
